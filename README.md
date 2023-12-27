3bffa4421b73a88db84ccd705c407df6930dda56
Tyler Davis <coding@tydavis.dev>
2023-12-27 15:50:54 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# white-elephant-number-distributor
+# White Elephant Number Distributor
+This program will allow you to input names, create a random order, and gives the user the option to print the results.
+
+## Usage
+Run main.py, then follow the on screen directions. If you have a large list of names, consider entering them into names.csv.
---
+++ main.py
@@ -0,0 +1,64 @@
+import csv
+import os.path
+import random
+import win32print
+import win32api
+
+names = []
+numbers = []
+
+if os.path.isfile('names.csv') == True:
+    with open('names.csv', 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        names = next(csvreader)
+        numberOfPeople = len(names)
+else:
+    numberOfPeople = int(input("Enter number of people: "))
+    for i in range(0, numberOfPeople):
+        ele = str(input())
+        names.append(ele)
+    with open('names.csv', 'w') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(names)
+
+if os.path.isfile('numbers.csv') == True:
+    with open('numbers.csv', 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        numbers = next(csvreader)
+    if len(numbers) != numberOfPeople:
+        numbers = random.sample(range(numberOfPeople), numberOfPeople)
+        numbers = [x + 1 for x in numbers]
+        with open('numbers.csv', 'w') as f:
+            csvwriter = csv.writer(f)
+            csvwriter.writerow(numbers)
+else:
+    numbers = random.sample(range(numberOfPeople), numberOfPeople)
+    numbers = [x+1 for x in numbers]
+    with open('numbers.csv', 'w') as f:
+        csvwriter = csv.writer(f)
+        csvwriter.writerow(numbers)
+
+dict = {names[i]: numbers[i] for i in range(len(names))}
+sortedDict = sorted(dict.items(), key=lambda item: item[1])
+with open("output.txt", "w") as f:
+    print("White Elephant Number Distribution", file=f)
+    for key, value in sortedDict:
+        print(value, ': ', key, file=f)
+        print(value, ': ', key)
+
+printResults = input('Do you want to print these results? (y/n): ').lower().strip() == 'y'
+
+if printResults is True:
+    printer_name = win32print.GetDefaultPrinter()
+    hPrinter = win32print.OpenPrinter(printer_name)
+    filename = "output.txt"
+    try:
+        hJob = win32print.StartDocPrinter(hPrinter, 1, ('PrintJobName', None, 'RAW'))
+        try:
+           win32api.ShellExecute(0, "print", filename, None, ".", 0)
+        finally:
+            win32print.EndDocPrinter(hPrinter)
+    finally:
+        win32print.ClosePrinter(hPrinter)
+else:
+    print()
