4572bd552ef422e1d8797323e8efc307176739fe
Tyler Davis <coding@tydavis.dev>
2024-01-02 18:46:12 -0800

    mkdir "white-elephant-number-distributor" and change version number

--- main.py
+++ main.py
@@ -9,43 +9,48 @@ import win32api
 names = []
 numbers = []
 
-outputFileName = "output.txt"
+file_exists = os.path.exists('./white-elephant-number-distributor')
+if not file_exists:
+    path = "./white-elephant-number-distributor"
+    os.mkdir(path)
 
-version = "v0.2.0"
+outputFileName = "./white-elephant-number-distributor/output.txt"
+
+version = "v0.3.0"
 print("You are using version", version, "\n")
 
 # Checks for if names.csv exists, and uses the values in it if it does. Creates a new file and fills it if not.
-if os.path.isfile('names.csv') == True:
-    with open('names.csv', 'r') as csvfile:
+if os.path.isfile('./white-elephant-number-distributor/names.csv') == True:
+    with open('./white-elephant-number-distributor/names.csv', 'r') as csvfile:
         csvreader = csv.reader(csvfile)
         names = next(csvreader)
         numberOfPeople = len(names)
-        print("Using values from names.csv.\n")
+        print("Using values from ./white-elephant-number-distributor/names.csv.\n")
 else:
     numberOfPeople = int(input("Enter number of people: "))
     print("Please enter the names of the people that are participating, pressing enter after each name:")
     for i in range(0, numberOfPeople):
         ele = str(input())
         names.append(ele)
-    with open('names.csv', 'w') as f:
+    with open('./white-elephant-number-distributor/names.csv', 'w') as f:
         csvwriter = csv.writer(f)
         csvwriter.writerow(names)
 
 # Checks for if numbers.csv exists, and uses the values in it if it does. Creates a new file and fills it if not.
-if os.path.isfile('numbers.csv') == True:
-    with open('numbers.csv', 'r') as csvfile:
+if os.path.isfile('./white-elephant-number-distributor/numbers.csv') == True:
+    with open('./white-elephant-number-distributor/numbers.csv', 'r') as csvfile:
         csvreader = csv.reader(csvfile)
         numbers = next(csvreader)
     if len(numbers) != numberOfPeople:
         numbers = random.sample(range(numberOfPeople), numberOfPeople)
         numbers = [x + 1 for x in numbers]
-        with open('numbers.csv', 'w') as f:
+        with open('./white-elephant-number-distributor/numbers.csv', 'w') as f:
             csvwriter = csv.writer(f)
             csvwriter.writerow(numbers)
 else:
     numbers = random.sample(range(numberOfPeople), numberOfPeople)
     numbers = [x+1 for x in numbers]
-    with open('numbers.csv', 'w') as f:
+    with open('./white-elephant-number-distributor/numbers.csv', 'w') as f:
         csvwriter = csv.writer(f)
         csvwriter.writerow(numbers)
     print("New numbers were generated.\n")
